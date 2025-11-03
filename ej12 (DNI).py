744306de98fd173755eae357a3791fc86041a3dc
bedez159 <bedez00123@gmail.com>
2025-11-03 12:00:59 +0100

    creacion de un progama de DNI

---
+++ ej12 (DNI).py
@@ -0,0 +1,23 @@
+"""
+Programa que devuelve la letra del DNI usando un contenedor de tipo diccionario 
+para mapear cada número con la letra correspondiente.
+La letra del dni se obtiene dividiendo el número del DNI entre 23.
+Se consideran todas las letras del abecedario en español salvo tres:
+la "Ñ", la "I" y la "O".
+"""
+
+DNI_letras = {
+    0: 'T', 1: 'R', 2: 'W', 3: 'A', 4: 'G', 5: 'M', 6: 'Y', 7: 'F',
+    8: 'P', 9: 'D', 10: 'X', 11: 'B', 12: 'N', 13: 'J', 14: 'Z',
+    15: 'S', 16: 'Q', 17: 'V', 18: 'H', 19: 'L', 20: 'C', 21: 'K', 22: 'E'
+}
+
+DNI = input("Dame tu DNI (los 8 dígitos): ")
+
+if len(DNI) != 8 or not DNI.isdigit():
+    print("⚠️ El DNI debe tener exactamente 8 dígitos numéricos.")
+else:
+    DNI_num = int(DNI)
+    resto = DNI_num % 23
+    letra = DNI_letras[resto]
+    print(f"✅ Tu DNI completo es:{DNI}{letra}")
